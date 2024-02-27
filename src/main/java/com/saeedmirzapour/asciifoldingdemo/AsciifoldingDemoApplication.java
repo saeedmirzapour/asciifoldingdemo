64d2293efd8a321075688543a27f0e731ee37e7d
Saeed Mirzapour <saeedmirzapour71@yahoo.com>
2024-02-27 15:36:09 +0330

    Initial Commit

---
+++ src/main/java/com/saeedmirzapour/asciifoldingdemo/AsciifoldingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.saeedmirzapour.asciifoldingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciifoldingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsciifoldingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saeedmirzapour/asciifoldingdemo/AsciifoldingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeedmirzapour.asciifoldingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsciifoldingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
